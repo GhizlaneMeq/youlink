1bf1cf10cb4ddb0fecc9eef33d4307cca410ef47
GhizlaneMeq <meqdarghizlane@gmail.com>
2024-04-04 10:04:28 +0000

    instalation du core ui pour les icones

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -8,6 +8,10 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+// Import all of CoreUI's JS
+import * as coreui from '@coreui/coreui'
+
+window.coreui = coreui
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
