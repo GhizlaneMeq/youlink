d0d7ffb385b2192f8fae0e9c1807809000a08883
GhizlaneMeq <meqdarghizlane@gmail.com>
2024-04-04 10:06:30 +0000

    factories

---
+++ database/migrations/2024_04_03_150744_create_exchanges_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('exchanges', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('requested_book_id')->constrained('books')->onDelete('cascade');
+            $table->foreignId('offered_book_id')->constrained('books')->onDelete('cascade');
+            $table->unsignedBigInteger('requester_id');
+            $table->unsignedBigInteger('offerer_id');
+            $table->enum('status', ['requested', 'accepted', 'completed', 'cancelled'])->default('requested');
+            $table->timestamps();
+
+            $table->foreign('requester_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('offerer_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('exchanges');
+    }
+};
